121f514da94e16b74da285571d4d810f72f35df1
Juank Suarez <camitrianasuarez@gmail.com>
2024-11-26 13:16:17 -0500

    v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PadreComponent } from './padre/padre.component';
+import { HeaderComponent } from "./header/header.component";
+import { ArticuloComponent } from "./articulo/articulo.component";
+import { FooterComponent } from "./footer/footer.component";
+import { NavComponent } from "./nav/nav.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [PadreComponent, HeaderComponent, ArticuloComponent, FooterComponent, NavComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'adso';
+  title = 'Clase';
+  nombre = 'Juank';
 }
+
